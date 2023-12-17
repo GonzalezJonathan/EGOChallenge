f37b32fd76a90da69a29583cec72b3e20764e781
jgonzalez <jarielg17@gmail.com>
2023-12-17 15:19:37 -0300

    EGOChallenge

---
+++ AgenciaEGO/MyApp/autos.py
@@ -0,0 +1,122 @@
+autos = [
+    {
+        "anio": 2019,
+        "modelo": "Etios",
+        "precio": 815900,
+        "tipo": "Auto",
+        "caracteristicas":{
+            "caracteristica":"caracteristica",
+            "descripcion":"descripcion"
+        }
+    },
+    {
+        "anio": 2020,
+        "modelo": "Yaris",
+        "precio": 1038900,
+        "tipo": "PickUps",
+        "caracteristicas":{
+            "caracteristica":"caracteristica",
+            "descripcion":"descripcion"
+        }
+    },
+    {
+        "anio": 2018,
+        "modelo": "Corolla",
+        "precio": 1430700,
+        "tipo": "Comercial",
+        "caracteristicas":{
+            "caracteristica":"caracteristica",
+            "descripcion":"descripcion"
+        }
+    },
+    {
+        "anio": 2019,
+        "modelo": "Prius",
+        "precio": 2882000,
+        "tipo": "SUVs",
+        "caracteristicas":{
+            "caracteristica":"caracteristica",
+            "descripcion":"descripcion"
+        }
+    },
+    {
+        "anio": 2018,
+        "modelo": "Camry",
+        "precio": 3753200,
+        "tipo": "Crossovers",
+        "caracteristicas":{
+            "caracteristica":"caracteristica",
+            "descripcion":"descripcion"
+        }
+    },
+    {
+        "anio": 2019,
+        "modelo": "Toyota 86",
+        "precio": 3812100,
+        "tipo": "Auto",
+        "caracteristicas":{
+            "caracteristica":"caracteristica",
+            "descripcion":"descripcion"
+        }
+    },   
+    {
+        "anio": 2020,
+        "modelo": "Innova",
+        "precio": 2596400,
+        "tipo": "PickUps",
+        "caracteristicas":{
+            "caracteristica":"caracteristica",
+            "descripcion":"descripcion"
+        }
+    },
+    {
+        "anio": 2019,
+        "modelo": "SW4",
+        "precio": 3236200,
+        "tipo": "Comerciales",
+        "caracteristicas":{
+            "caracteristica":"caracteristica",
+            "descripcion":"descripcion"
+        }
+    }, 
+    {
+        "anio": 2019,
+        "modelo": "RAV4",
+        "precio": 3170200,
+        "tipo": "SUVs",
+        "caracteristicas":{
+            "caracteristica":"caracteristica",
+            "descripcion":"descripcion"
+        }
+    },            
+    {
+        "anio": 2017,
+        "modelo": "Land Cruiser Prado",
+        "precio": 815900,
+        "tipo": "Crossovers",
+        "caracteristicas":{
+            "caracteristica":"caracteristica",
+            "descripcion":"descripcion"
+        }
+    }, 
+    {
+        "anio": 2018,
+        "modelo": "Land Cruiser 200",
+        "precio": 7873100,
+        "tipo": "Auto",
+        "caracteristicas":{
+            "caracteristica":"caracteristica",
+            "descripcion":"descripcion"
+        }
+    },
+    {
+        "anio": 2020,
+        "modelo": "Hilux",
+        "precio": 1507000,
+        "tipo": "PickUps",
+        "caracteristicas":{
+            "caracteristica":"caracteristica",
+            "descripcion":"descripcion"
+        }
+    },              
+]
